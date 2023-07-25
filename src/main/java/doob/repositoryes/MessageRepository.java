452f9d9411b948e2215ff41067ab524d3e8557c4
AlexOak <dubovec995@gmail.com>
2023-07-25 13:32:23 +0200

    create

--- src/main/java/doob/repositoryes/MessageRepository.java
+++ src/main/java/doob/repositoryes/MessageRepository.java
@@ -1,28 +1,66 @@
 package doob.repositoryes;
 
 
+import doob.entity.Dialog;
 import doob.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
+import doob.entity.User;
+import doob.mysql.Connector;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Integer> {
+public class MessageRepository {
 
-    @Override
-    List<Message> findAll();
+    @Autowired
+    private Connector connector;
+    public static final String path = "E:/Code/messages/";
 
-    @Override
-    <S extends Message> S save(S entity);
 
-    @Override
-    Optional<Message> findById(Integer integer);
+    public String getPath(User sender, User recipient) {
+        String pathForCreate = path + sender.getId() + "_" + recipient.getId();
+        File folder = new File(pathForCreate);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        return pathForCreate;
+    }
 
-    @Override
-    void delete(Message entity);
+    public boolean save(String context, User sender, User recipient) {
+        File file = new File(getPath(sender, recipient));
+        try (FileWriter writer = new FileWriter(file, true)) {
+            writer.write(LocalDateTime.now().toString() + ": ");
+            writer.write(context);
+            writer.write("\n");
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 
-    @Override
-    void deleteById(Integer integer);
+    public Dialog buildDialog(User sender, User recipient) {
+        Set<Message> messages = new HashSet<>();
+        try (FileReader fr = new FileReader(getPath(sender, recipient))) {
+            BufferedReader br = new BufferedReader(fr);
+            while (br.readLine() != null) {
+                Message message = new Message();
+                message.setContext(br.readLine());
+                message.setSender(sender);
+                message.setRecipient(recipient);
+                messages.add(message);
+            }
+            Dialog dialog = new Dialog();
+            dialog.setMessages(messages);
+            return dialog;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Dialog();
+        }
+
+    }
 }
